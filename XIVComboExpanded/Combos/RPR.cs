a2831f6a8b0a10c98d9ad85963724daf99f2a222
Grammernatzi <ultragamer2007@gmail.com>
2021-12-04 06:08:03 -0800

    I realized that checking for cooldown was pointless after doing more research

--- XIVComboExpanded/Combos/RPR.cs
+++ XIVComboExpanded/Combos/RPR.cs
@@ -145,8 +145,7 @@
         {
             if (actionID == RPR.ArcaneCircle)
             {
-                var circleCD = GetCooldown(RPR.ArcaneCircle);
-                if (circleCD.CooldownRemaining != 0 && HasEffect(RPR.Buffs.ImmortalSacrifice) && level >= RPR.Levels.PlentifulHarvest)
+                if (HasEffect(RPR.Buffs.ImmortalSacrifice) && level >= RPR.Levels.PlentifulHarvest)
                     return RPR.PlentifulHarvest;
             }
 
